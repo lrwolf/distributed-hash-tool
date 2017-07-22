76101e68091b55d693aeecfb0896adce777c12c6
Lucas <lucas.wolf@gmail.com>
2017-07-22 15:56:30 -0700

    Add files via upload

--- project.clj
+++ project.clj
@@ -1,7 +1,19 @@
-(defproject quil-workflow "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject distributed-hash-tool "0.1.0-SNAPSHOT"
+  :description "A tool to demonstrate how a distribute hash table works."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.6.0"]])
+                 [quil "2.6.0"]
+                 [org.clojure/clojurescript "1.9.473"]]
+
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds [{:source-paths ["src"]
+             :compiler
+             {:output-to "js/main.js"
+              :output-dir "out"
+              :main "distributed_hash_tool.core"
+              :optimizations :none
+              :pretty-print true}}]})
